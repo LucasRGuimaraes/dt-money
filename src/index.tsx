67d5676e9fcdf9540d0cc3fc3a1897993e513442
LucasRGuimaraes <lukasmarinho2008@gmail.com>
2022-08-07 23:42:58 -0300

    Add render API Data on the transactionsTable

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model, Server } from 'miragejs';
 import { App } from './App';
 
 createServer({
+
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'dinheirinho do trampo hihi',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')  
+        },
+        {
+          id: 2,
+          title: 'Cartão de crédito',
+          type: 'withdraw',
+          category: 'dinheirinho indo embora :(',
+          amount: 2300,
+          createdAt: new Date('2021-02-14 10:00:00')  
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Job',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
