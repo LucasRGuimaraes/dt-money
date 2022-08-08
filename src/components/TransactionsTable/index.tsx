efaab56d5e10d81eca37e166588ef4e27894e651
LucasRGuimaraes <lukasmarinho2008@gmail.com>
2022-08-08 17:44:19 -0300

    Add Context and Hook for transactions list, Add render in summary, Add render in TransactionsTable

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
-interface transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
+
 
 export function TransactionsTable () {
-
-    const [transactions, setTransactions] = useState<transaction[]>([])
-
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    }, [])
+    const { transactions } = useTransactions()
+    
 
     return (
         <Container>
@@ -33,20 +20,20 @@ export function TransactionsTable () {
                 </thead>
 
                 <tbody>
-                    {transactions.map(transaction => {
+                    {transactions.map(transactions => {
                         return (
-                            <tr key={transaction.id}>
-                                <td>{transaction.title}</td>
-                                <td className={transaction.type}>
+                            <tr key={transactions.id}>
+                                <td>{transactions.title}</td>
+                                <td className={transactions.type}>
                                     {new Intl.NumberFormat('pt-Br', {
                                         style: 'currency',
                                         currency: 'BRL'
-                                    }).format(transaction.amount)}
+                                    }).format(transactions.amount)}
                                 </td>
-                                <td>{transaction.category}</td>
+                                <td>{transactions.category}</td>
                                 <td>
                                     {new Intl.DateTimeFormat('pt-Br').format(
-                                        new Date(transaction.createdAt)
+                                        new Date(transactions.createdAt)
                                     )}
                                 </td>
                             </tr>
